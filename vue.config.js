e988680bf60ee6dd026efe7e251fa53524bf8026
ZhangXiongWei <445080336@qq.com>
2019-07-01 10:36:11 +0800

    测试合并

--- vue.config.js
+++ vue.config.js
@@ -6,11 +6,7 @@ function resolve(dir) {
     return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'vue Admin Template' // page title
-    // If your port is set to 80,
-    // use administrator privileges to execute the command line.
-    // For example, Mac: sudo npm run
-const port = 8080 // dev port
+const name = defaultSettings.title || 'Admin' // page title
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -21,13 +17,13 @@ module.exports = {
      * In most cases please use '/' !!!
      * Detail: https://cli.vuejs.org/config/#publicpath
      */
-    publicPath: './',
+    publicPath: process.env.NODE_ENV === 'development' ? '/' : './', //生产环境需要改为'./'
     outputDir: 'dist',
     assetsDir: 'static',
     lintOnSave: process.env.NODE_ENV === 'development',
     productionSourceMap: false,
     devServer: {
-        port: port,
+        port: 8080,
         open: true,
         overlay: {
             warnings: false,
