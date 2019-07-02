ebdc7ba577b3e941610c170dea6fc1c33b685f8d
ZhangXiongWei <445080336@qq.com>
2019-07-02 18:06:20 +0800

    管理页排版开始

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,40 +1,84 @@
 <template>
   <div class="dashboard-container">
-    <el-tabs tab-position="left" v-model="tab">
-      <el-tab-pane label="Users">
-        <div class="tab-page"></div>
-      </el-tab-pane>
-      <el-tab-pane label="Ungrouped users">
-        <div class="tab-page"></div>
-      </el-tab-pane>
-    </el-tabs>
-    <el-button @click="guan">
-      点我
-    </el-button>
+    <el-container>
+      <el-header>
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="Users" name="first">
+            
+          </el-tab-pane>
+          <el-tab-pane label="Ungrouped users" name="second"></el-tab-pane>
+          <el-tab-pane label="用户组" name="third"></el-tab-pane>
+        </el-tabs>
+      </el-header>
+      <el-aside width="200px" style="border-right:1px solid #ddd;">
+        <el-row class="tac">
+          <el-col :span="24">
+            <h5>自定义颜色</h5>
+            <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                  <template slot="title">分组一</template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                  <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                  <template slot="title">选项4</template>
+                  <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-container>
+        <el-header height="80px" style="text-align:center">
+          {{title}}
+        </el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+  import { mapGetters } from "vuex";
 
-export default {
-  name: "Dashboard",
-  data() {
-    return {
-      tab:''
+  export default {
+    name: "Dashboard",
+    data() {
+      return {
+        title: 'title'
+      }
+    },
+    computed: {},
+    methods: {
+      guan() {
+        this.tab = null;
+      }
     }
-  },
-  computed: {},
-  methods:{
-    guan(){
-      this.tab = null;
-    }
-  }
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-.dashboard-container{
-  
-}
+  .dashboard-container {}
 </style>
