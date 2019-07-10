91df57a7c94946f3cc5fbad9b01524c0879e1f9c
li1991-hello@163.com <li1991-hello@163.com>
2019-07-10 17:37:54 +0800

    [新增-李剑]:视频审核界面

--- src/components/VideoPlayer/index.vue
+++ src/components/VideoPlayer/index.vue
@@ -1,38 +1,39 @@
 <template>
-  <div>
-    <img id="imgId" style="width:850px;height:500px" :style="{display:!videoPlayerIsShow?'block':'none'}">
-    <div id="videoPlayer" :style="{display:videoPlayerIsShow?'block':'none'}">Loading the player...</div>
-    <el-row>
-      <el-col :span="2">
-        <el-button :icon="playerControls.stateIcon" @click="playerPlay"></el-button>
-      </el-col>
-      <el-col :span="18">
-
-        <el-slider v-model="playerPercentage" :show-tooltip='false'></el-slider>
-
-        {{playerFormatCurrentPostion}} / {{playerFormatDuration}}&nbsp;音量:{{playerVolume}}
-        <el-button-group>
-          <el-button type="primary" icon="el-icon-edit" @click="changeVolume('add')">+</el-button>
-          <el-button type="primary" icon="el-icon-share" @click="changeVolume('sub')">-</el-button>
-          <el-button type="primary" icon="el-icon-share" @click="changeFullscreen">全</el-button>
-
-        </el-button-group>
-      </el-col>
-      <el-col :span="2">
-        <button @click="demo">截图</button>
-
-      </el-col>
-    </el-row>
-    <img id="imgId1">
-  </div>
+    <div class="player-box">
+        <img id="imgVision" class="img-vision" :style="{display:!videoPlayerIsShow?'block':'none'}">
+        <div id="videoPlayer" :style="{display:videoPlayerIsShow?'block':'none'}">Loading the player...</div>
+        <el-row id="playerToolbar">
+          <el-col :span="1" class="bar-item">  
+            <el-button class="btn-item btn-play" :icon="playerControls.stateIcon" @click="playerPlay"></el-button>
+          </el-col>
+           <el-col :span="18" class="bar-item">
+             <span class="text">{{playerFormatCurrentPostion}}</span>
+             <div class="slider"><el-slider v-model="playerPercentage" :show-tooltip='false' @change="handleSliderChange"></el-slider></div>
+             <span class="text">{{playerFormatDuration}}</span>
+          </el-col>
+           <el-col :span="5" class="bar-item">
+            <span style="color:#fff;">音量:{{playerVolume}}</span>
+            <el-button-group class="btn-group">
+              <el-button class="btn-item" @click="changeVolume('sub')"><img :src="iconLower"/></el-button>
+              <el-button class="btn-item" @click="changeVolume('add')"><img :src="iconIncrease"/></el-button>
+              <el-button class="btn-item" @click="handleMark"><img :src="iconMark"/></el-button>
+              <el-button class="btn-item" @click="changeFullscreen"><img :src="iconFullscreen"/></el-button>
+            </el-button-group>
+          </el-col>
+        </el-row>        
+    </div>
 </template>
 
 <script>
-  import VideoCapture from 'video-capture'
-  import kscreenshot from 'kscreenshot'
-  import { setInterval } from 'timers'
+   import VideoCapture from 'video-capture'
+   import kscreenshot from 'kscreenshot'
+   import { setInterval } from 'timers';
+   import iconIncrease from './icons/icon-increase.png';
+   import iconLower from './icons/icon-lower.png';
+   import iconFullscreen from './icons/icon-fullscreen.png';
+   import iconMark from './icons/icon-mark.png';
 
-  export default {
+   export default {
     name: 'VideoPlayer',
     props: {
       url: {
@@ -41,183 +42,229 @@
       }
 
     },
-    data() {
+    data () {
       return {
-        videoPlayerIsShow: true,
-        videoPlayer: null,
-        kscreenshot: null,
-        fullscreen: false,
-        playerControls: {
-          stateIcon: 'el-icon-video-pause'
-        },
-        playerVolume: 50,
-        playerDuration: 0,
-        playerFormatDuration: '00:00',
-        playerCurrentPostion: 0,
-        playerFormatCurrentPostion: '00:00',
-        playerPercentage: 0
-
+        iconIncrease:iconIncrease,
+        iconLower:iconLower,
+        iconFullscreen:iconFullscreen,
+        iconMark:iconMark,
+         videoPlayerIsShow:true,
+         videoPlayer:null,
+         kscreenshot:null,
+         fullscreen:false,
+         playerControls:{
+           stateIcon:"el-icon-video-pause"
+         },
+         playerVolume:50,
+         playerDuration:0,
+         playerFormatDuration:"00:00",
+         playerCurrentPostion:0,
+         playerFormatCurrentPostion:"00:00",
+         playerPercentage:0
       }
     },
     created() {
     },
-    mounted() {
+    mounted (){
       console.log(this.url)
-      let _self = this
-      this.videoPlayer = jwplayer('videoPlayer').setup({
-        flashplayer: '/lib/jwplayer-v6.1.2972/player.swf',
-        file: this.url,
-        height: '500',
-        width: '850',
-        abouttext: '',
-        primary: 'flash',
-        controls: true,
-        controlbar: 'bottom',
-        autostart: true,//自动播放
-        events: {
-          onComplete: function() {
-            _self.playerControls.stateIcon = 'el-icon-video-play'
-            console.log('播放结束!!!')
-          },
-          onVolume: function() {
-            console.log('声音大小改变!!!')
-          },
-          onReady: function() {
-            console.log('准备就绪!!!')
-          },
-          onPlay: function() {
-            console.log('开始播放!!!')
-            this.setVolume(_self.playerVolume)
-            _self.playerFormatDuration = (_self.formatSeconds(this.getDuration()))
-            _self.playerDuration = this.getDuration()
-            _self.playerVolume = this.getVolume()
-            var playerThis = this
-            setInterval(function() {
-              _self.playerCurrentPostion = playerThis.getPosition()
-              _self.playerFormatCurrentPostion = _self.formatSeconds(_self.playerCurrentPostion)
-              _self.playerPercentage = ((_self.playerCurrentPostion / _self.playerDuration) * 100)
-            }, 1000)
-          },
-          onPause: function() {
-            console.log('暂停!!!')
-          },
-          onBufferChange: function() {
-            console.log('缓冲改变!!!')
-          },
-          onBufferFull: function() {
-            console.log('视频缓冲完成!!!')
-          },
-          onError: function(obj) {
-            console.log('播放器出错!!!' + obj.message)
-          },
-          onFullscreen: function(obj) {
-            if (obj.fullscreen) {
-              console.log('全屏')
-              _self.fullscreen = true
-            } else {
-              _self.fullscreen = false
-            }
-          },
-          onMute: function(obj) {
-            console.log('静音/取消静音')
-          }
-        }
+      let _self=this;
+       this.videoPlayer=jwplayer("videoPlayer").setup({
+                flashplayer: "/lib/jwplayer-v6.1.2972/player.swf",
+                file:this.url,
+                height:"100%",
+                width:"100%",
+                abouttext:"",
+                primary: "flash",
+                controls:false,
+                controlbar:"bottom",
+                autostart:true,//自动播放
+                events: {
+                  onComplete: function () { 
+                    _self.playerControls.stateIcon = 'el-icon-video-play';   
+                    console.log("播放结束!!!");
+                  },
+                  onVolume: function () { console.log("声音大小改变!!!"); },
+                  onReady: function () { console.log("准备就绪!!!"); },
+                  onPlay: function () { 
+                    console.log("开始播放!!!"); 
+                    this.setVolume(_self.playerVolume)
+                    _self.playerFormatDuration=(_self.formatSeconds(this.getDuration()))
+                    _self.playerDuration=this.getDuration();
+                    _self.playerVolume=this.getVolume();
+                    var playerThis=this;
+                    setInterval(function(){
+                      _self.playerCurrentPostion=playerThis.getPosition(); 
+                      _self.playerFormatCurrentPostion=_self.formatSeconds(_self.playerCurrentPostion);
+                      _self.playerPercentage=((_self.playerCurrentPostion/_self.playerDuration)*100)
+                    },1000)
+                  },
+                  onPause: function () { console.log("暂停!!!"); },
+                  onBufferChange: function () {
+                     console.log("缓冲改变!!!"); 
+                  },
+                  onBufferFull: function () { console.log("视频缓冲完成!!!"); },
+                  onError: function (obj) { console.log("播放器出错!!!" + obj.message); },
+                  onFullscreen: function (obj) { 
+                    if (obj.fullscreen) { 
+                      console.log("全屏"); 
+                      _self.fullscreen=true;
+                    } else { 
+                      _self.fullscreen=false;
+                    } 
+                  },
+                  onMute: function (obj) { console.log("静音/取消静音") }
+              }
+        
+            });
+            
 
-      })
-
+     
     },
-    methods: {
-      async demo() {
-        let _self = this
-        console.log(this.videoPlayer)
+    methods:{
+      handleSliderChange(data){
+        console.log(data);
+      },
+      async handleMark(){
+        let _self=this;
+        console.log(this.videoPlayer);
         console.log(this.videoPlayer.getDuration())
         console.log(this.videoPlayer.getPosition())
-        this.videoPlayer.pause(true)
-        let { dataURL, width, height } = await new VideoCapture(this.url).capture(this.videoPlayer.getPosition())
-        document.getElementById('imgId').src = dataURL
-        this.videoPlayerIsShow = false
-
-        this.kscreenshot = new kscreenshot({})
-        this.kscreenshot.endCB = function(data) {
-          document.getElementById('imgId1').src = data
-          _self.videoPlayerIsShow = true
+        this.videoPlayer.pause(true);
+        let { dataURL, width, height } = await new VideoCapture(this.url).capture(this.videoPlayer.getPosition());
+        document.getElementById("imgVision").src = dataURL;
+        this.videoPlayerIsShow=false;  
+        this.kscreenshot=new kscreenshot({})
+        this.kscreenshot.endCB=function(data){
+          var obj = {
+            imgUrl:data,
+            currentPosition:_self.videoPlayer.getPosition()
+          };
+          _self.$emit("getCutImg",obj);
+          _self.videoPlayerIsShow=true;
         }
-        this.kscreenshot.cancelCB = function() {
+        this.kscreenshot.cancelCB=function(){
           console.log(111)
         }
-        this.kscreenshot.startScreenShot()
+        this.kscreenshot.startScreenShot();
       },
-
-      playerPlay: function() {
-        if (this.videoPlayer.getState() != 'PLAYING') {
-          this.videoPlayer.play(true)
-          this.playerControls.stateIcon = 'el-icon-video-pause'
-        } else {
-          this.videoPlayer.play(false)
-          this.playerControls.stateIcon = 'el-icon-video-play'
-        }
+      playerPlay:function(){
+        if (this.videoPlayer.getState() != 'PLAYING') {  
+              this.videoPlayer.play(true);  
+              this.playerControls.stateIcon = 'el-icon-video-pause';  
+          } else {  
+              this.videoPlayer.play(false);  
+              this.playerControls.stateIcon = 'el-icon-video-play';  
+          }  
       },
-      changeVolume: function(type) {
-        if ('add' == type) {
-          if (this.playerVolume < 100) {
-            this.playerVolume += 5
+      changeVolume:function(type){
+        if('add'==type){
+          if(this.playerVolume<100){
+            this.playerVolume+=5;
           }
-        } else if ('sub' == type) {
-          if (this.playerVolume > 0) {
-            this.playerVolume -= 5
+        }else if('sub'==type){
+          if(this.playerVolume>0){
+            this.playerVolume-=5;
           }
         }
         this.videoPlayer.setVolume(this.playerVolume)
 
       },
-      changeFullscreen: function() {
+      changeFullscreen:function(){
         this.videoPlayer.setFullscreen(true)
       },
       formatSeconds(value) {
-        var theTime = parseInt(value)// 秒
-        var middle = 0// 分
-        var hour = 0// 小时
+          var theTime = parseInt(value);// 秒
+          var middle= 0;// 分
+          var hour= 0;// 小时
 
-        if (theTime > 60) {
-          middle = parseInt(theTime / 60)
-          theTime = parseInt(theTime % 60)
-          if (middle > 60) {
-            hour = parseInt(middle / 60)
-            middle = parseInt(middle % 60)
+          if(theTime > 60) {
+              middle= parseInt(theTime/60);
+              theTime = parseInt(theTime%60);
+              if(middle> 60) {
+                  hour= parseInt(middle/60);
+                  middle= parseInt(middle%60);
+              }
+          }
+          var result="00:00";
+          theTime=parseInt(theTime);
+          if(theTime<10){
+            result = "0"+theTime;
+          }else{
+            result = theTime;
           }
-        }
-        var result = '00:00'
-        theTime = parseInt(theTime)
-        if (theTime < 10) {
-          result = '0' + theTime
-        } else {
-          result = theTime
-        }
-        if (middle > 0) {
-          middle = parseInt(middle)
-          if (middle < 10) {
-            result = '0' + middle + ':' + result
-          } else {
-            result = '' + middle + ':' + result
+          if(middle > 0) {
+              middle=parseInt(middle);
+              if(middle<10){
+                result = "0"+middle+":"+result;
+              }else{
+                result = ""+middle+":"+result;
 
+              }
+          }
+          if(hour> 0) {
+              result = ""+parseInt(hour)+":"+result;
+          }
+          if((result+"").split(":").length==1){
+            result="00:"+result;
           }
-        }
-        if (hour > 0) {
-          result = '' + parseInt(hour) + ':' + result
-        }
-        if ((result + '').split(':').length == 1) {
-          result = '00:' + result
-        }
-        return result
+          return result;
       }
-
+ 
     }
   }
 </script>
 
 
 <style lang="scss">
-  #videoCheck {
-
-  }
+    .player-box{
+      width:100%;
+      height:100%;
+      position:relative;
+      .img-vision{
+        width:100%;
+        height:100%;
+        position:absolute;
+        top:0;
+        left:0;
+      }
+    }
+    #playerToolbar{
+      height:38px;
+      line-height: 38px;
+      width:100%;
+      background-color: transparent;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      .bar-item{
+        text-align:center;
+        height:38px;
+        .text{
+          float:left;
+          width:10%;
+          color:#fff;
+          line-height:38px;
+          text-align:center;
+        }
+        .slider{
+          float:left;
+          width:80%;
+          color:#fff;
+        }
+      }
+      .btn-item{
+        border:none;
+        border-radius:none;
+        background:transparent;
+        padding:0;
+        color:#fff;
+        margin-left:6px;
+      }
+      .btn-play{
+        font-size:30px;
+        i{
+          font-weight:bold;
+        }
+      }
+    }
 </style>
--- src/views/video/videoCheck.vue
+++ src/views/video/videoCheck.vue
@@ -1,38 +1,169 @@
 <template>
-  <div id="videoCheck">
+    <div id="videoCheck">
+        <el-row class="page">
+            <el-col :span="15" class="page-left">
+                <div class="video-player">
+                    <!-- 播放器 -->
+                    <video-player :url="url" @getCutImg="getCutImg"/>
+                </div>
+                <div class="video-list">
+                    <!-- 视频项目列表 -->
+                    <ul class="mark-list">
+                        <li class="item" v-for="(item,index) in imgList" :key="index">
+                            <img :src="item.imgUrl" @click="zoomImg(item.imgUrl)"/>
+                        </li>
+                    </ul>
+                </div>
+            </el-col>
+            <el-col :span="9" class="page-right">
+                <div class="video-info">
+                    <!-- 视频基本信息 -->
+                    <div>
+                        <img src=""/>
+                    </div>
+                    <div>
+                        <p>版本链接：www.baidu.com</p>
+                        <p>版本：初始版本</p>
+                        <p>状态：最新</p>
+                        <p>资产类型：上传视频</p>
+                        <p>发布者：JSon</p>
+                        <p>已发布：7小时前</p>
+                    </div>
+                </div>
+                <div class="video-tabs">
+                    <!-- 视频tab页（Notes、Versions） -->
+                    <el-tabs v-model="activeTab" @tab-click="handleTabClick">
+                        <el-tab-pane label="标注" name="first">标注</el-tab-pane>
+                        <el-tab-pane label="版本" name="second">版本</el-tab-pane>
+                    </el-tabs>
+                </div>
+                <div class="video-comment">
+                    <!-- 视频标注列表 -->
+                    <ul>
+                        <li>
+                            <div></div>
+                            <div>
+                                <p><span>JSon.li</span><span>7小时前</span></p>
+                                <p>标注正确，还不错！</p>
+                                <p>
+                                    <img/>
+                                    <span>新项目/初始版本</span>
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
 
-    <div class=“left”>
-      <video-player :url="url"/>
-    </div>
-    <div class="right">
-
-    </div>
-  </div>
+                </div>
+            </el-col>
+        </el-row>
+        <zoom-img ref="zoomImg"/>
+     </div>
 </template>
 
 <script>
-  import VideoPlayer from '@/components/VideoPlayer'
-  import kscreenshot from 'kscreenshot'
-
-  export default {
-    components: { VideoPlayer },
-    data() {
+import VideoPlayer from '@/components/VideoPlayer'
+import kscreenshot from 'kscreenshot'
+import ZoomImg from '@/components/ZoomImg'
+export default {
+    components: {VideoPlayer,ZoomImg},
+    data () {
       return {
-        url: 'http://localhost:8080/banner01.mp4'
+          url:"http://localhost:8080/banner01.mp4",
+          imgList:[],
+          zoomImgUrl:"",
+          activeTab: 'first'
       }
     },
     created() {
 
     },
-    mounted() {
+    mounted (){
 
     },
-    methods: {}
-  }
+    methods:{
+       getCutImg(obj){
+           this.imgList.push(obj);
+           console.log(this.imgList);
+       },
+       zoomImg(imgUrl){
+           this.$refs.zoomImg.zoomImg(imgUrl);
+       },
+       handleTabClick(tab, event) {
+        console.log(tab, event);
+      }
+
+    }
+}
 </script>
 
-<style lang="scss">
-  #videoCheck {
-
-  }
+<style lang="scss" scoped>
+    #videoCheck{
+        width:100%;
+        height:100%;
+        background-color:#fff;
+        .page{
+            width:100%;
+            height:100%;
+            min-width: 1500px;
+            background-color: #eeeeee;
+            .page-left{
+                height:100%;
+                width:62%;
+                margin-right:0.5%;
+                .video-player{
+                    width: 100%;
+                    height:70%;
+                    background:#fff;
+                }
+                .video-list{
+                    width: 100%;
+                    height:28.6%;
+                    margin-top: 1%;
+                    background: #fff;
+                }
+            }
+            .page-right{
+                height:100%;
+                // margin-left: 0.5%;
+                .video-info{
+                    padding: 0 10px;
+                    height: 30%;
+                    margin-bottom: 10px;
+                    background: #fff;
+                }
+                .video-tabs{
+                    padding: 0 10px;
+                    background: #fff;
+                    max-height: 30%;
+                    margin-bottom: 10px;
+                    min-height: 10%;
+                }
+                .video-comment{
+                    padding: 0 10px;
+                    min-height: 40%;
+                    background: #fff;
+                    max-height: 60%;
+                    height: 57%;
+                }
+            }
+        }
+    }
+    .mark-list{
+        width: 100%;
+        height: 100%;
+        padding: 0 10px;
+        margin: 0;
+        list-style: none;
+        .item{
+            float:left;
+            width: 100px;
+            height: 100px;
+            margin-right: 12px;
+            margin: 12px 12px 12px 0;
+            img{
+                width: 100%;
+                height:100%;
+            }
+        }
+    }
 </style>
