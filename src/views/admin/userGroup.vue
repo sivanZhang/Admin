5d617868d9c7705998d04f1dcbbe030a077dcf86
mryfairy <18338991598@163.com>
2019-07-24 17:11:19 +0800

    用户列表样式修改

--- src/views/admin/userGroup.vue
+++ src/views/admin/userGroup.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="userGroup">
     <el-container>
-        <el-header style="padding-bottom:0px">
+        <el-header style="padding:0px;margin: 0px">
           <el-radio-group v-model="radio" size="small">
             <el-radio-button :label="1">所有用户</el-radio-button>
             <el-radio-button :label="2">未分组</el-radio-button>
           </el-radio-group>
         </el-header>
-        <el-main style="padding-top: 0px">
-          <users-table :UserList="UserList"></users-table>
+        <el-main style="padding: 0px">
+          <users-table :UserList="UserList" ></users-table>
         </el-main>
 
     </el-container>
