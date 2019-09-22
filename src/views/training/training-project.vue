342d8797efb24b15697936e46298528e088059c0
mryfairy <18339881598@163.com>
2019-09-22 17:05:50 +0800

    /*mry*/实训项目路由修改；store存储实训列表，创建、删除实训项目刷新

--- src/views/training/training-project.vue
+++ src/views/training/training-project.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="training-project">
-    <project :trainingProject="trainingProject" />
+    <project :trainingProject="trainingProject"  />
+    <!-- {{trainingProject}} -->
   </div>
 </template>
 
 <script>
-import { getTrainingProject } from "@/api/training";
+// import { getTrainingProject } from "@/api/training";
 import project from "@/views/overview/project";
+import { mapState } from "vuex";
 export default {
   name: "training-project",
   components: { project },
+  computed:{
+    ...mapState("trainingStatus",["trainingProject"])
+  },
   data() {
     return {
-      trainingProject: []
+      // trainingProject: []
     };
   },
   watch: {},
   methods: {
-    getTraining() {
-      getTrainingProject().then(({ data }) => {
-        if (data.status === 0) {
-          this.trainingProject = [...data.msg];
-        }
-      });
-    }
+    // getTraining() {
+    //   getTrainingProject().then(({ data }) => {
+    //     if (data.status === 0) {
+    //       this.trainingProject = [...data.msg];
+    //     }
+    //   });
+    // }
   },
   created() {
-    this.getTraining();
+    //this.getTraining();
+    this.$store.dispatch("trainingStatus/get_TrainProject")
   }
 };
 </script>
