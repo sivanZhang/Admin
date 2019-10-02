1a440014fc9bf055860f086fb5b2e4384965367a
zhangxiongwei <445080336@qq.com>
2019-10-02 12:29:30 +0800

    esline

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     publicPath: './',
     outputDir: 'dist',
     assetsDir: 'static',
-    lintOnSave: process.env.NODE_ENV === 'development',
+    lintOnSave: false, //process.env.NODE_ENV === 'development'
     productionSourceMap: false,
     devServer: {
         port: 8080,
