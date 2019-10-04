07a5ea4b5feac21a21d7be5f93d1754aa80c8627
mryfairy <18339881598@163.com>
2019-10-04 17:46:05 +0800

    /*mry*/删除角色二次确认调试信息修改

--- src/views/admin/roles.vue
+++ src/views/admin/roles.vue
@@ -323,7 +323,7 @@ export default {
     },
     //删除角色
     delRole(todo) {
-      this.$confirm("此操作将永久删除该资产, 是否继续?", "提示", {
+      this.$confirm("此操作将永久删除该角色, 是否继续?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning"
