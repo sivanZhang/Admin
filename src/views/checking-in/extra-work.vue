9221554d500167f2dc88811ae068b6ac030bfa9e
hanyi <1737403035@qq.com>
2019-10-04 17:44:54 +0800

    加班管理和插件搜索，批量删除

--- src/views/checking-in/extra-work.vue
+++ src/views/checking-in/extra-work.vue
@@ -133,7 +133,7 @@ export default {
      },
     //批量删除
     deletList(){
-      this.$confirm("此操作将永久删除用户,是否继续?", "提示", {
+      this.$confirm("此操作将永久删除申请,是否继续?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning"
