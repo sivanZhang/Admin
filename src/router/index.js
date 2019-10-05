b945e466a9a7e03d330ce96205ec5a6c7ae607cd
mryfairy <18339881598@163.com>
2019-10-05 21:02:33 +0800

    /*mry*/router冲突解决

--- src/router/index.js
+++ src/router/index.js
@@ -26,38 +26,7 @@ meta: {
  */
 
 //涉及权限的动态加载路由
-export const asyncRoutes = [{
-        path: '/checking-in',
-        component: Layout,
-        redirect: 'noRedirect',
-        alwaysShow: true,
-        meta: {
-            title: '我的考勤',
-            icon: 'kq'
-        },
-        children: [{
-                path: 'extra-work',
-                name: 'extra-work',
-                component: () =>
-                    import ('@/views/checking-in/extra-work'),
-                meta: {
-                    title: '加班管理',
-                    icon: 'jiaban'
-                }
-            },
-            {
-                path: 'approve-extra-work',
-                name: 'approve-extra-work',
-                component: () =>
-                    import ('@/views/checking-in/approve-extra-work'),
-                meta: {
-                    title: '加班审批',
-                    icon: 'jbsp',
-                    roles: 'manage_overtime_approve'
-                }
-            }
-        ]
-    }, {
+export const asyncRoutes = [ {
         path: '/task',
         component: Layout,
         redirect: 'noRedirect',
