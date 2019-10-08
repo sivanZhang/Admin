e022cd5e5d0f40745bc61ee97f766984f3c0ded1
zhangxiongwei <445080336@qq.com>
2019-10-08 11:10:36 +0800

    1

--- src/views/projects/components/training-member.vue
+++ src/views/projects/components/training-member.vue
@@ -1,56 +1,5 @@
 <!-- 实训成员 -->
-<template>
-  <div id="training" ref="drawer-parent">
-    <el-table
-      ref="trainingMenber"
-      :data="trainingMenber.slice((currentPage-1)*pageSize,currentPage*pageSize)"
-      
-      :span-method="objectSpanMethod"
-    >
-      <el-table-column label="所属分组" prop="name"></el-table-column>
-      <el-table-column label="实训人员">
-        <template slot-scope="scope">
-          <div @click="openRanking()" class="links">
-            {{scope.row.username}}
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column label="学校" prop="school"></el-table-column>
-      <el-table-column label="参与任务数量" prop="task_num" align="center"></el-table-column>
-      <el-table-column label="考勤" align="center">
-        <el-table-column label="正常考勤" prop="normal"></el-table-column>
-        <el-table-column label="迟到次数" prop="late"></el-table-column>
-        <el-table-column label="早退次数" prop="leave_early"></el-table-column>
-        <el-table-column label="旷课次数" prop="absentee"></el-table-column>
-      </el-table-column>
-      <el-table-column label="总得分" prop="total_score"></el-table-column>
-      <el-table-column label="排名" prop="range"></el-table-column>
-    </el-table>
-    <div class="block" style="text-align: center;margin-top:10px">
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="currentPage"
-        :page-sizes="pageSizeList"
-        :page-size="pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="trainingMenber.length"
-      ></el-pagination>
-    </div>
-    <Drawer
-      scrollable
-      closable
-      draggable
-      v-model="isRankingShow"
-      :transfer="false"
-      :mask="false"
-      :inner="isInner"
-      :title="usernameTitle"
-    >
-    排名雷达图
-    </Drawer>
-  </div>
-</template>  
+
 <script>
 import thumbtackMixin from "@/utils/thumbtack-mixin";
 export default {
@@ -60,8 +9,8 @@ export default {
   props: ["trainingMenber"],
   data() {
     return {
-      usernameTitle:'',
-      isRankingShow:false,
+      usernameTitle: "",
+      isRankingShow: false,
       currentPage: 1,
       pageSize: 20,
       pageSizeList: [10, 20, 50, 100]
@@ -75,8 +24,8 @@ export default {
   },
   methods: {
     // 打开个人排名抽屉
-    openRanking(){
-      this.isRankingShow = true
+    openRanking() {
+      this.isRankingShow = true;
     },
     Group(name) {
       return this.trainingMenber.filter(item => item.name == name).length;
@@ -128,8 +77,56 @@ export default {
   created() {}
 };
 </script>
+<template>
+  <div id="training" ref="drawer-parent">
+    <el-table
+      ref="trainingMenber"
+      :data="trainingMenber.slice((currentPage-1)*pageSize,currentPage*pageSize)"
+      :span-method="objectSpanMethod"
+    >
+      <el-table-column label="所属分组" prop="name"></el-table-column>
+      <el-table-column label="实训人员">
+        <template slot-scope="scope">
+          <div @click="openRanking()" class="links">{{scope.row.username}}</div>
+        </template>
+      </el-table-column>
+      <el-table-column label="学校" prop="school"></el-table-column>
+      <el-table-column label="参与任务数量" prop="task_num" align="center"></el-table-column>
+      <el-table-column label="考勤" align="center">
+        <el-table-column label="正常考勤" prop="normal"></el-table-column>
+        <el-table-column label="迟到次数" prop="late"></el-table-column>
+        <el-table-column label="早退次数" prop="leave_early"></el-table-column>
+        <el-table-column label="旷课次数" prop="absentee"></el-table-column>
+      </el-table-column>
+      <el-table-column label="总得分" prop="total_score"></el-table-column>
+      <el-table-column label="排名" prop="range"></el-table-column>
+    </el-table>
+    <div class="block" style="text-align: center;margin-top:10px">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="pageSizeList"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="trainingMenber.length"
+      ></el-pagination>
+    </div>
+    <Drawer
+      scrollable
+      closable
+      draggable
+      v-model="isRankingShow"
+      :transfer="false"
+      :mask="false"
+      :inner="isInner"
+      :title="usernameTitle"
+    >排名雷达图</Drawer>
+  </div>
+</template>  
+
 <style lang='scss' scoped>
-.links{
+.links {
   color: #2d8cf0;
   cursor: pointer;
 }
