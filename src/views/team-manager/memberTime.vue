5c097e96c17d07c05ae8191521060934021f5786
mryfairy <18339881598@163.com>
2019-10-17 21:47:18 +0800

    /*mry*/人员任务统计修改

--- src/views/team-manager/memberTime.vue
+++ src/views/team-manager/memberTime.vue
@@ -104,7 +104,7 @@ export default {
           },
           visualMap: {
             min: 0,
-            max: 10,
+            max: 50,
             calculable: true,
             orient: "horizontal",
             left: "center",
