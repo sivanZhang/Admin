2cc1978a28b4625022198aac5fcd1bf70e965270
jeawy <461882709@qq.com>
2019-12-14 11:54:08 +0800

    修改头像

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -12,7 +12,7 @@
       </template>
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img src="@/assets/logo.jpg" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
