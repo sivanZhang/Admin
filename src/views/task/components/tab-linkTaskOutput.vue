b9057cfc4c7218d3d2ec8eb1a730ede51dff973b
H1012 <1737403035@qq.com>
2019-12-24 16:40:01 +0800

    //李洁茹   关联任务输出  添加环节名称字段

--- src/views/task/components/tab-linkTaskOutput.vue
+++ src/views/task/components/tab-linkTaskOutput.vue
@@ -1,78 +1,74 @@
 <template>
   <div id="linkTaskOutput">
-      <!-- 关联任务输出 -->
-      <el-row>
-        <el-col :span="10" >
-          <div style="padding:5px 0px">
-            <label for>上一环节任务输出</label>
-          </div>
-        </el-col>
-      </el-row>
-      <el-table
-            :data="TaskOutput"
-            :border="true"
-            :header-cell-style="{background:'#eef1f6',color:'#606266'}"
-            style="width:100%"
-           
-          >
-            <el-table-column prop="task_name" label="任务名称" width="80"></el-table-column>
-             <el-table-column prop="task_content" label="任务内容"></el-table-column>
-           
-            <el-table-column prop="path" label="输出文件的路径" width="200"></el-table-column>
-      </el-table>
-      <el-row>
-        <el-col :span="10" >
-          <div style="padding:15px 0px">
-            <label for>镜头的其他任务输出</label>
-          </div>
-        </el-col>
-      </el-row>
-      <el-table
-            :data="AssetOutput"
-            :border="true"
-            :header-cell-style="{background:'#eef1f6',color:'#606266'}"
-            style="width:100%"
-           
-          >
-            <el-table-column prop="task_name" label="任务名称" width="80"></el-table-column>
-             <el-table-column prop="task_content" label="任务内容"></el-table-column>
-           
-            <el-table-column prop="path" label="输出文件的路径" width="200"></el-table-column>
-      </el-table>
+    <!-- 关联任务输出 -->
+    <el-row>
+      <el-col :span="10">
+        <div style="padding:5px 0px">
+          <label for>上一环节任务输出</label>
+        </div>
+      </el-col>
+    </el-row>
+    <el-table
+      :data="TaskOutput"
+      :border="true"
+      :header-cell-style="{background:'#eef1f6',color:'#606266'}"
+      style="width:100%"
+    >
+      <el-table-column prop="link_content" label="环节名称" width="80"></el-table-column>
+      <el-table-column prop="task_name" label="任务名称" width="80"></el-table-column>
+      <el-table-column prop="task_content" label="任务内容"></el-table-column>
+      <el-table-column prop="path" label="输出文件的路径" width="200"></el-table-column>
+    </el-table>
+    <el-row>
+      <el-col :span="10">
+        <div style="padding:15px 0px">
+          <label for>镜头的其他任务输出</label>
+        </div>
+      </el-col>
+    </el-row>
+    <el-table
+      :data="AssetOutput"
+      :border="true"
+      :header-cell-style="{background:'#eef1f6',color:'#606266'}"
+      style="width:100%"
+    >
+      <el-table-column prop="link_content" label="环节名称" width="80"></el-table-column>
+      <el-table-column prop="task_name" label="任务名称" width="80"></el-table-column>
+      <el-table-column prop="task_content" label="任务内容"></el-table-column>
+
+      <el-table-column prop="path" label="输出文件的路径" width="200"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import { getLinkOutput }  from "@/api/task";
+import { getLinkOutput } from "@/api/task";
 export default {
-   name: "linkTaskOutput",
-   data(){
-     return{
-          TaskOutput:[],
-          AssetOutput:[],
-     }
-   },
-   created(){
-      // this.getlinkTaskOutput();
-   },
-   methods:{
-     //父组件直接通过$refs[组件ref值].getlinkTaskOutput(task的id)即可请求列表，切换筛选时自动调用的
-     getlinkTaskOutput(task_id){
+  name: "linkTaskOutput",
+  data() {
+    return {
+      TaskOutput: [],
+      AssetOutput: []
+    };
+  },
+  created() {
+    // this.getlinkTaskOutput();
+  },
+  methods: {
+    //父组件直接通过$refs[组件ref值].getlinkTaskOutput(task的id)即可请求列表，切换筛选时自动调用的
+    getlinkTaskOutput(task_id) {
       //  console.log(11111)
       //  console.log(task_id)
-       getLinkOutput({task_id:task_id}).then(({ data })=>{
-         this.TaskOutput = data.other_task_output;
-         this.AssetOutput= data.parent_link_output;
-
-       })
-     }
-   }
-}
-
+      getLinkOutput({ task_id: task_id }).then(({ data }) => {
+        this.TaskOutput = data.other_task_output;
+        this.AssetOutput = data.parent_link_output;
+      });
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
 
 
