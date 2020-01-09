75416064458020ee7b523dec6859140fd030fbf6
jeawy <461882709@qq.com>
2020-01-09 20:43:03 +0800

    插件修改

--- src/views/myTask/components/detail.vue
+++ src/views/myTask/components/detail.vue
@@ -151,13 +151,14 @@ export default {
     handelClick() {
       const self = this
       new QWebChannel(qt.webChannelTransport, function({ objects }) {
+        appManager.text = `path@${data.msg}@${self.taskRecord.task_id}`
         var appManager = objects.app_manager
         getDirs({
           id: self.taskRecord.task_id,
           working: '',
           os: self.os
         }).then(({ data }) => {
-          appManager.text = `path@${data.msg}`
+          appManager.text = `path@${data.msg}@${self.taskRecord.task_id}`
         })
       })
     },
